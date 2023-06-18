308c05466268b7be78c7e2772abde9b30bad75da
David_Monheit <93082922+DMM771@users.noreply.github.com>
2023-06-18 12:44:00 +0300

    Update Local_Train_Color.py

--- AutoBot/aditional files/Model_Trainers_Scripts/Local_Train_Color.py
+++ AutoBot/aditional files/Model_Trainers_Scripts/Local_Train_Color.py
@@ -8,117 +8,50 @@ from keras.preprocessing.image import ImageDataGenerator
 
 def color_net(num_classes):
     # placeholder for input image
-    input_image = Input(shape=(224, 224, 3))
-    # ============================================= TOP BRANCH ===================================================
-    # first top convolution layer
-    top_conv1 = Convolution2D(filters=48, kernel_size=(11, 11), strides=(4, 4),
-                              input_shape=(224, 224, 3), activation='relu')(input_image)
-    top_conv1 = BatchNormalization()(top_conv1)
-    top_conv1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(top_conv1)
+    i_image = Input(shape=(224, 224, 3))
 
-    # second top convolution layer
-    # split feature map by half
-    top_top_conv2 = Lambda(lambda x: x[:, :, :, :24])(top_conv1)
-    top_bot_conv2 = Lambda(lambda x: x[:, :, :, 24:])(top_conv1)
+    # function for making layers
+    def my_layer_maker(n_filters, filter_size, prev_layer, strides=(1,1), activation='relu', max_pool=False, batch_norm=False):
+        conv_layer = Convolution2D(filters=n_filters, kernel_size=filter_size, strides=strides, activation=activation, padding='same')(prev_layer)
+        if batch_norm:
+            conv_layer = BatchNormalization()(conv_layer)
+        if max_pool:
+            conv_layer = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_layer)
+        return conv_layer
 
-    top_top_conv2 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_top_conv2)
-    top_top_conv2 = BatchNormalization()(top_top_conv2)
-    top_top_conv2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(top_top_conv2)
+    # top branch
+    top_conv1 = my_layer_maker(n_filters=48, filter_size=(11, 11), prev_layer=i_image, strides=(4, 4), max_pool=True, batch_norm=True)
+    top_top_conv2 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, :24])(top_conv1), max_pool=True, batch_norm=True)
+    top_bot_conv2 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, 24:])(top_conv1), max_pool=True, batch_norm=True)
+    top_conv3 = my_layer_maker(n_filters=192, filter_size=(3, 3), prev_layer=Concatenate()([top_top_conv2, top_bot_conv2]))
+    top_top_conv4 = my_layer_maker(n_filters=96, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, :96])(top_conv3))
+    top_bot_conv4 = my_layer_maker(n_filters=96, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, 96:])(top_conv3))
+    top_top_conv5 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=top_top_conv4, max_pool=True)
+    top_bot_conv5 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=top_bot_conv4, max_pool=True)
 
-    top_bot_conv2 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_bot_conv2)
-    top_bot_conv2 = BatchNormalization()(top_bot_conv2)
-    top_bot_conv2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(top_bot_conv2)
+    # bottom branch
+    bottom_conv1 = my_layer_maker(n_filters=48, filter_size=(11, 11), prev_layer=i_image, strides=(4, 4), max_pool=True, batch_norm=True)
+    bottom_top_conv2 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, :24])(bottom_conv1), max_pool=True, batch_norm=True)
+    bottom_bot_conv2 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, 24:])(bottom_conv1), max_pool=True, batch_norm=True)
+    bottom_conv3 = my_layer_maker(n_filters=192, filter_size=(3, 3), prev_layer=Concatenate()([bottom_top_conv2, bottom_bot_conv2]))
+    bottom_top_conv4 = my_layer_maker(n_filters=96, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, :96])(bottom_conv3))
+    bottom_bot_conv4 = my_layer_maker(n_filters=96, filter_size=(3, 3), prev_layer=Lambda(lambda x: x[:, :, :, 96:])(bottom_conv3))
+    bottom_top_conv5 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=bottom_top_conv4, max_pool=True)
+    bottom_bot_conv5 = my_layer_maker(n_filters=64, filter_size=(3, 3), prev_layer=bottom_bot_conv4, max_pool=True)
 
-    # third top convolution layer
-    # concat 2 feature map
-    top_conv3 = Concatenate()([top_top_conv2, top_bot_conv2])
-    top_conv3 = Convolution2D(filters=192, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_conv3)
-
-    # fourth top convolution layer
-    # split feature map by half
-    top_top_conv4 = Lambda(lambda x: x[:, :, :, :96])(top_conv3)
-    top_bot_conv4 = Lambda(lambda x: x[:, :, :, 96:])(top_conv3)
-
-    top_top_conv4 = Convolution2D(filters=96, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_top_conv4)
-    top_bot_conv4 = Convolution2D(filters=96, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_bot_conv4)
-
-    # fifth top convolution layer
-    top_top_conv5 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_top_conv4)
-    top_top_conv5 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(top_top_conv5)
-
-    top_bot_conv5 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        top_bot_conv4)
-    top_bot_conv5 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(top_bot_conv5)
-
-    # ============================================= TOP BOTTOM ===================================================
-    # first bottom convolution layer
-    bottom_conv1 = Convolution2D(filters=48, kernel_size=(11, 11), strides=(4, 4),
-                                 input_shape=(227, 227, 3), activation='relu')(input_image)
-    bottom_conv1 = BatchNormalization()(bottom_conv1)
-    bottom_conv1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(bottom_conv1)
-
-    # second bottom convolution layer
-    # split feature map by half
-    bottom_top_conv2 = Lambda(lambda x: x[:, :, :, :24])(bottom_conv1)
-    bottom_bot_conv2 = Lambda(lambda x: x[:, :, :, 24:])(bottom_conv1)
-
-    bottom_top_conv2 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_top_conv2)
-    bottom_top_conv2 = BatchNormalization()(bottom_top_conv2)
-    bottom_top_conv2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(bottom_top_conv2)
-
-    bottom_bot_conv2 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_bot_conv2)
-    bottom_bot_conv2 = BatchNormalization()(bottom_bot_conv2)
-    bottom_bot_conv2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(bottom_bot_conv2)
-
-    # third bottom convolution layer
-    # concat 2 feature map
-    bottom_conv3 = Concatenate()([bottom_top_conv2, bottom_bot_conv2])
-    bottom_conv3 = Convolution2D(filters=192, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_conv3)
-
-    # fourth bottom convolution layer
-    # split feature map by half
-    bottom_top_conv4 = Lambda(lambda x: x[:, :, :, :96])(bottom_conv3)
-    bottom_bot_conv4 = Lambda(lambda x: x[:, :, :, 96:])(bottom_conv3)
-
-    bottom_top_conv4 = Convolution2D(filters=96, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_top_conv4)
-    bottom_bot_conv4 = Convolution2D(filters=96, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_bot_conv4)
-
-    # fifth bottom convolution layer
-    bottom_top_conv5 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_top_conv4)
-    bottom_top_conv5 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(bottom_top_conv5)
-
-    bottom_bot_conv5 = Convolution2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same')(
-        bottom_bot_conv4)
-    bottom_bot_conv5 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(bottom_bot_conv5)
-
-    # ======================================== CONCATENATE TOP AND BOTTOM BRANCH =================================
+    # connect both branches
     conv_output = Concatenate()([top_top_conv5, top_bot_conv5, bottom_top_conv5, bottom_bot_conv5])
-
-    # Flatten
     flatten = Flatten()(conv_output)
 
-    # Fully-connected layer
-    FC_1 = Dense(units=4096, activation='relu')(flatten)
-    FC_1 = Dropout(0.6)(FC_1)
-    FC_2 = Dense(units=4096, activation='relu')(FC_1)
-    FC_2 = Dropout(0.6)(FC_2)
-    output = Dense(units=num_classes, activation='softmax')(FC_2)
+    # add fully-connected and last layer
+    f1 = Dense(units=4096, activation='relu')(flatten)
+    f1 = Dropout(0.6)(f1)
+    f2 = Dense(units=4096, activation='relu')(f1)
+    f2 = Dropout(0.6)(f2)
+    output = Dense(units=num_classes, activation='softmax')(f2)
 
-    model = Model(inputs=input_image, outputs=output)
-    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
-    # sgd = SGD(lr=0.01, momentum=0.9, decay=0.0005, nesterov=True)
+    model = Model(inputs=i_image, outputs=output)
+    sgd = SGD(learning_rate=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
@@ -126,15 +59,17 @@ def color_net(num_classes):
 
 img_rows, img_cols = 224, 224
 num_classes = 15
-batch_size = 32
-nb_epoch = 5
+batch_size = 4
+nb_epoch = 20
 
 # initialise model
 model = color_net(num_classes)
 
 filepath = 'color_weights_test.hdf5'
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False,
+checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_grads=False,
                           write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None,
                           embeddings_data=None, update_freq='batch')
 callbacks_list = [checkpoint, tensorboard]
@@ -150,24 +85,25 @@ test_datagen = ImageDataGenerator(rescale=1. / 255,
                                   dtype='float32')
 
 training_set = train_datagen.flow_from_directory(
-    'dataset/train/',
+    'Datasets/Color_DataSet/train',
     target_size=(img_rows, img_cols),
     batch_size=batch_size,
     class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory(
-    'dataset/test/',
+    'Datasets/Color_DataSet/test',
     target_size=(img_rows, img_cols),
     batch_size=batch_size,
     class_mode='categorical')
 label_map = (test_set.class_indices)
 
-model.fit_generator(
-    training_set,
-    steps_per_epoch=2892,
-    epochs=nb_epoch,
-    validation_data=test_set,
-    validation_steps=665,
-    callbacks=callbacks_list)
+model.fit(
+        training_set,
+        steps_per_epoch=len(training_set),
+        epochs=nb_epoch,
+        validation_data=test_set,
+        validation_steps=len(test_set),
+        callbacks=callbacks_list)
+
 
 model.save('color_model.h5')
